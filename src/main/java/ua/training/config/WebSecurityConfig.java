e5a488291b606de4d34635e8e4be98274ed35970
Stepess <stepanersh@gmail.com>
2019-04-23 22:59:41 +0300

    secure

--- src/main/java/ua/training/config/WebSecurityConfig.java
+++ src/main/java/ua/training/config/WebSecurityConfig.java
@@ -44,7 +44,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+        http.formLogin().loginPage("/login")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .logoutUrl("/logout")
                 .and()
                 .authorizeRequests()
                 .antMatchers("/periodicals/me").authenticated()
@@ -52,8 +56,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/periodicals/**", "/periodicals/mine").authenticated()
                 .anyRequest().permitAll()
                 // to require https
-        .and()
+                .and()
                 .requiresChannel()
-                .antMatchers("/spitter/form").requiresSecure();
+                .antMatchers("/spitter/form").requiresSecure()
+                .and()
+                .httpBasic()
+                .realmName("periodicals")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(2419200)
+                .key("periodicals13");
+        /*<input id="remember_me" name="remember-me" type="checkbox"/>
+<label for="remember_me" class="inline">Remember me</label>*/
     }
 }
