a67ac372bc9debfc118df88b1c3c6738090aa4d2
Stepess <stepanersh@gmail.com>
2019-04-23 22:07:04 +0300

    fix configs

--- src/main/java/ua/training/config/WebSecurityConfig.java
+++ src/main/java/ua/training/config/WebSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -34,14 +36,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER").and()
-                .withUser("admin").password("1111").roles("USER", "ADMIN");
+                .withUser("user").password(encoder.encode("password")).roles("USER").and()
+                .withUser("admin").password(encoder.encode("1111")).roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.formLogin()
+                .and()
+                .authorizeRequests()
                 .antMatchers("/periodicals/me").authenticated()
                 .antMatchers(HttpMethod.POST, "/periodicals").authenticated()
                 .antMatchers("/periodicals/**", "/periodicals/mine").authenticated()
