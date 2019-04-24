9d2a9bd95768b7f2dc617c40de0c1684a87fc701
Stepan Yershov <stepan.yershov@ticketmaster.com>
2019-04-24 10:32:09 +0300

    11

--- src/main/java/ua/training/config/AppConfig.java
+++ src/main/java/ua/training/config/AppConfig.java
@@ -1,8 +1,48 @@
 package ua.training.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import
+        org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import
+        org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
+
+    @Profile("development")
+    @Bean
+    public DataSource embeddedDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+
+    @Profile("QA")
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:tcp://localhost/~/periodicals");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        dataSource.setInitialSize(5);
+        dataSource.setMaxTotal(10);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
 }
---
+++ src/main/java/ua/training/data/JdbcUserRepository.java
@@ -0,0 +1,48 @@
+package ua.training.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.stereotype.Repository;
+import ua.training.model.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class JdbcUserRepository implements UserRepository {
+
+    private JdbcOperations jdbcOperations;
+
+    public static final String FIND_USER_BY_USERNAME =
+            "SELECT * FROM users WHERE username = ?";
+
+    public static final String INSERT_USER =
+            "INSERT INTO users (username, password, email, firstName, lastName) " +
+                    "values (?, ?, ?, ?, ?)";
+
+    @Autowired
+    public JdbcUserRepository(JdbcOperations jdbcOperations) {
+        this.jdbcOperations = jdbcOperations;
+    }
+
+    @Override
+    public User save(User user) {
+         jdbcOperations.update(INSERT_USER, user.getUsername(), user.getLastName(),
+                user.getEmail(), user.getFirstName(), user.getLastName());
+         return user;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return jdbcOperations.queryForObject(FIND_USER_BY_USERNAME, this::mapUser, username);
+    }
+
+    private User mapUser(ResultSet rs, int rows) throws SQLException {
+        return new User(rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("password"),
+                rs.getString("email"),
+                rs.getString("firstName"),
+                rs.getString("lastName"));
+    }
+}
