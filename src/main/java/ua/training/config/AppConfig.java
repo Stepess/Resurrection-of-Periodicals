abbc8bb7f29eaeb3e6bb299a6ede38a098407a31
Stepess <stepanersh@gmail.com>
2019-04-28 13:17:39 +0300

    11.2

--- src/main/java/ua/training/config/AppConfig.java
+++ src/main/java/ua/training/config/AppConfig.java
@@ -2,14 +2,18 @@ package ua.training.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class AppConfig {
@@ -51,6 +55,22 @@ public class AppConfig {
         dataSource.setInitialSize(5);
         dataSource.setMaxTotal(10);
         return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean ls = new LocalSessionFactoryBean();
+        ls.setDataSource(dataSource);
+        ls.setPackagesToScan("ua.training.model");
+        Properties properties = new Properties();
+        properties.setProperty("dialect", "org.hibernate.dialect.H2Dialect");
+        ls.setHibernateProperties(properties);
+        return ls;
+    }
+
+    @Bean
+    public BeanPostProcessor persistenceTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
     }
 
     @Bean
---
+++ src/main/java/ua/training/data/HibernateUserRepository.java
@@ -0,0 +1,59 @@
+package ua.training.data;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ua.training.model.User;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.io.Serializable;
+import java.util.List;
+
+@Repository
+public class HibernateUserRepository implements UserRepository{
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public HibernateUserRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    private Session getCurentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public User save(User user) {
+        Session session = getCurentSession();
+        Serializable id = session.save(user);
+        return new User((Long) id, user.getUsername(), user.getPassword(), user.getEmail(),
+                user.getFirstName(),user.getLastName());
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        Session session = getCurentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = criteriaBuilder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.where(criteriaBuilder.equal(root.get("username"), username));
+        return session.createQuery(query).getSingleResult();
+    }
+
+    @Override
+    public List<User> findAll(long max, int count) {
+        Session session = getCurentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = criteriaBuilder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.where(criteriaBuilder.le(root.get("id"), max));
+        Query<User> userQuery = session.createQuery(query);
+        userQuery.setMaxResults(count);
+        return userQuery.getResultList();
+    }
+}
