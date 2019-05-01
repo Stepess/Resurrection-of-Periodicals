1278493c1799c1ac7fa8761a2d222fea7846bb21
Stepess <stepanersh@gmail.com>
2019-05-01 11:44:32 +0300

    add implementation

--- src/main/java/ua/training/data/JpaUserRepository.java
+++ src/main/java/ua/training/data/JpaUserRepository.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 import ua.training.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
@@ -27,15 +28,49 @@ public class JpaUserRepository implements UserRepository {
     }
 
     @Override
-    public User findByUsername(String username) {
-
-        return entityManager.find(User.class, 1L);
+    public User update(User user) {
+        User userDB = findById(user.getId());
+        userDB.setUsername(user.getUsername());
+        userDB.setPassword(user.getPassword());
+        userDB.setFirstName(user.getFirstName());
+        userDB.setLastName(user.getLastName());
+        userDB.setEmail(user.getEmail());
+        return user;
     }
 
     @Override
-    public List<User> findAll(long max, int count)
-    {
-        entityManager.
-        return null;
+    public User delete(User user) {
+        entityManager.remove(user);
+        return user;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        ParameterExpression<String> parameter = cb.parameter(String.class, "username");
+        CriteriaQuery<User> criteriaQuery = cb.createQuery(User.class);
+        Root<User> from = criteriaQuery.from(User.class);
+        criteriaQuery.select(from).where(cb.equal(from.get("username"), parameter));
+        TypedQuery<User> query = entityManager.createQuery(criteriaQuery);
+        query.setParameter(parameter, username);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public List<User> findAll(long max, int count) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = cb.createQuery(User.class);
+        Root<User> from = criteriaQuery.from(User.class);
+        ParameterExpression<Long> id = cb.parameter(Long.class);
+        criteriaQuery.select(from).where(cb.le(from.get("id"), id));
+        TypedQuery<User> query = entityManager.createQuery(criteriaQuery);
+        return query.setParameter(id, max)
+                .setMaxResults(count)
+                .getResultList();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
     }
 }
--- src/main/java/ua/training/data/UserRepository.java
+++ src/main/java/ua/training/data/UserRepository.java
@@ -6,10 +6,16 @@ import java.util.List;
 
 public interface UserRepository {
 
-    User save(User user);
+    List<User> findAll(long max, int count);
+
+    User findById(Long id);
 
     User findByUsername(String username);
 
-    List<User> findAll(long max, int count);
+    User save(User user);
+
+    User update(User user);
+
+    User delete(User user);
 
 }
